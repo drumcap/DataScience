c94aaedde369726cb60b87848ffb9b53aa39aa01
moonkwoo <0707moon@naver.com>
2016-11-21 19:58:22 +0900

    complete flask & mongodb

--- exercise/DataBase/0_flask_mongodb.py
+++ exercise/DataBase/0_flask_mongodb.py
@@ -37,3 +37,100 @@ for i in range(0, count_num):
 result = daum_api.find()
 for doc in result:
     print doc
+
+
+"""find city whose population is greater than 10,000,000"""
+from pymongo import MongoClient
+from exer_connection_db import mongo_account
+mongo = MongoClient('ec2-54-213-224-243.us-west-2.compute.amazonaws.com' ,27017)
+mongo.the_database.authenticate(mongo_account['id'], mongo_account['pw'], source = 'test')
+zip_collection = mongo.test.zip
+
+pipelines = []
+pipelines.append({'$group' : {'_id' :'$state', 'pop' : {'$sum' : '$pop'}}})
+pipelines.append({'$match' : {'pop' : {'$gte' : 10 * 1000 * 1000}}})
+
+result = zip_collection.aggregate(pipelines)
+for doc in result:
+    print doc
+
+
+from collections import defaultdict
+
+result = zip_collection.find()
+
+state_pop = defaultdict(lambda : 0)
+
+for doc in result:
+    state_pop[doc['state']] += doc['pop']
+
+for i, j in state_pop.items():
+    if j > 10 * 1000 * 1000:
+        print i, j
+
+new_state_pop = {k : v for k, v in state_pop.items() if v > 10 * 1000 * 1000}
+print new_state_pop
+
+
+"""find average poopulation in states"""
+from pymongo import MongoClient
+from exer_connection_db import mongo_account
+
+mongo = MongoClient('ec2-54-213-224-243.us-west-2.compute.amazonaws.com', 27017)
+mongo.the_database.authenticate(mongo_account['id'], mongo_account['pw'], source = 'test')
+zip_collection = mongo.test.zip
+
+pipelines = []
+pipelines.append({'$group' : {'_id' :{'state' : '$state', 'city' : '$city'}, 'pop' : {'$sum' : '$pop'}}})
+pipelines.append({'$group' :{'_id' : '$_id.state', 'avgpop' : {'$avg' : '$pop'}}})
+
+result = zip_collection.aggregate(pipelines)
+for doc in result:
+    print doc
+
+
+"""find maximum,  minimun population in states"""
+from pymongo import MongoClient
+from exer_connection_db import mongo_account
+
+mongo = MongoClient('ec2-54-213-224-243.us-west-2.compute.amazonaws.com', 27017)
+mongo.the_database.authenticate(mongo_account['id'], mongo_account['pw'], source = 'test')
+zip_collection = mongo.test.zip
+
+pipelines = []
+pipelines.append({'$group' : {'_id' : {'state' : "$state", 'city' : "$city"}, 'pop' : {'$sum' : "$pop"}}})
+pipelines.append({'$sort' : {'pop' : 1}})
+pipelines.append({'$group' : {'_id' : "$_id.state", 'biggestcity' : {'$last' : "$_id.city"}, 'biggestpop' : {'$last' : "$pop"}, 'smallestcity' : {'$first' : "$_id.city"}, 'smallestpop' : {'$first' : "$pop"}}})
+pipelines.append({'$project' : {'_id' : 0, 'state' : "$_id", 'biggestcity' : {'name' : "$biggestcity", 'pop' : "$biggestpop"}, 'smallestcity' : {'name' : "$smallestcity", 'pop' : '$smallestpop'}}})
+
+
+result = zip_collection.aggregate(pipelines)
+for doc in result:
+    print doc
+
+
+"""find the number of restaurant per borough"""
+mongo = MongoClient('ec2-54-213-224-243.us-west-2.compute.amazonaws.com' ,27017)
+mongo.the_database.authenticate(mongo_account['id'], mongo_account['pw'], source = 'test')
+rest = mongo.test.rest
+
+pipelines = []
+pipelines.append({'$group' : {'_id' : "$borough", 'count' : {'$sum' : 1}}})
+
+result = rest.aggregate(pipelines)
+for doc in result:
+    print doc
+
+
+"""find the number of 'Brazilian' restaurant per zipcode"""
+mongo = MongoClient('ec2-54-213-224-243.us-west-2.compute.amazonaws.com' ,27017)
+mongo.the_database.authenticate(mongo_account['id'], mongo_account['pw'], source = 'test')
+rest = mongo.test.rest
+
+pipelines = []
+pipelines.append({'$match' : {'cuisine' : 'Brazilian'}})
+pipelines.append({'$group' : {'_id' : "$address.zipcode", 'cnt' : {'$sum' : 1}}})
+
+result = rest.aggregate(pipelines)
+for doc in result:
+    print doc
